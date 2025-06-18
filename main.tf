99d6d3e94c7ea89f6ef5471d8919ddeae1ba396a
Sharmila Nagarajan <sharmeeh@outlook.com.au>
2025-06-18 15:55:51 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "storageaccountname"
+  name                     = "18062025stoacc"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
