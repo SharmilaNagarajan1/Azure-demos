068c6b22bc6ff8a4504e604a617e57959dceb47f
Sharmila Nagarajan <sharmeeh@outlook.com.au>
2025-06-18 15:27:25 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,9 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.53.0"
+      version = "~> 4.8.0"
     }
   }
+required_version = ">=1.9.0"
+
 }
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
