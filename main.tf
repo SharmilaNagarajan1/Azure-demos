edef43b254e36b955f97ceafb5c28332d86bbee0
Sharmila Nagarajan <sharmeeh@outlook.com.au>
2025-06-18 15:19:46 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.53.0"
+    }
+  }
+}
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+   //skip_provider_registration = true 
+  features {}
+
+
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "West Europe"
